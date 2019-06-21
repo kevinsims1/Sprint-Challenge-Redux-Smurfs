51115ab63aff895a35e63b14c0aead46fd7fcb5c
Kevin Sims <kevinsims@Kevins-MacBook-Pro.local>
2019-06-21 10:49:21 -0700

    rendering smurf

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
 import './App.css';
+import { connect } from "react-redux";
+import Loader from "react-loader-spinner";
+
+import { fetchSmurfs, addSmurf } from "../actions";
 /*
  to wire this component up you're going to need a few things.
  I'll let you do this part on your own. 
@@ -7,16 +11,42 @@ import './App.css';
  `How do I ensure that my component links the state to props?`
  */
 class App extends Component {
+ 
+  
   render() {
+    console.log(this.props.smurfs)
     return (
       <div className="App">
-        <h1>SMURFS! 2.0 W/ Redux</h1>
-        <div>Welcome to your Redux version of Smurfs!</div>
-        <div>Start inside of your `src/index.js` file!</div>
-        <div>Have fun!</div>
+          <div>
+            {this.props.smurfs.map(smurf => (
+              <div className="card">
+                <div className="card-body">
+                  <h1>{smurf.name}</h1>
+                  <h3>{smurf.age}</h3>
+                  <h4>{smurf.height}</h4>
+                </div>
+              </div>
+            ))}           
+          </div>            
       </div>
-    );
-  }
+    )
+}
+componentDidMount() {
+  this.props.fetchSmurfs();
+}
 }
 
-export default App;
+const mapStateToProps = state => {
+  console.log(state);
+
+  return{
+  smurfs: state.smurfs
+  }
+};
+
+export default connect(
+  mapStateToProps,
+  {
+    fetchSmurfs
+  }
+)(App);
--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -1,18 +1,50 @@
 /*
   Be sure to import in all of the action types from `../actions`
 */
+import {
+  FETCH_SMURFS_START,
+  FETCH_SMURFS_SUCCESS,
+  FETCH_SMURFS_FAILURE,
+  ADD_SMURF,
+  ADD_SMURF_FAILURE
+} from "../actions";
 
-/*
- Your initial/default state for this project could *Although does not have to* look a lot like this
- {
+
+ const initialState = {
    smurfs: [],
-   fetchingSmurfs: false
-   addingSmurf: false
-   updatingSmurf: false
-   deletingSmurf: false
+   fetchingSmurfs: false,
+   addingSmurf: false,
+   updatingSmurf: false,
+   deletingSmurf: false,
    error: null
  }
-*/
+
+ const reducer = (state = initialState, action) => {
+   switch(action.type) {
+     case FETCH_SMURFS_START:
+       return {
+         ...state,
+         fetchingSmurfs: true,
+         error: null
+       }
+      case FETCH_SMURFS_SUCCESS:
+        return {
+          ...state,
+          fetchingSmurfs: false,
+          smurfs: action.payload
+        }
+      case FETCH_SMURFS_FAILURE:
+        return {
+          ...state,
+          error: action.payload,
+          fetchingSmurfs: false
+        }
+      default: 
+        return state
+   }
+ }
+
+ export default reducer;
 
 /*
   You'll only need one smurf reducer for this project.
